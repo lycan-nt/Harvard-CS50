b7212722c74163be2ad9e699259f572f652c94d1
Felipe D. santos <lycan.felipe@gmail.com>
2020-04-21 09:22:25 -0300

    Week 1

---
+++ Week 1/cough0.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("cough \n");
+    printf("cough \n");
+    printf("cough \n");
+}
---
+++ Week 1/cough1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+void cough(void);
+
+int main(void) {
+    for(int i=0; i < 3; i++) {
+        cough();
+    }
+}
+
+void cough(void) {
+    printf("Cough\n");
+}
