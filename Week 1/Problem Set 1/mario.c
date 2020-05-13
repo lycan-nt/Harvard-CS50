8e0096b323efaf22b109e48666b8a2818b5a2bc2
Felipe D. Santos <lycan.felipe@gmail.com>
2020-05-13 16:30:51 -0300

    Mario.c

---
+++ Week 1/Problem Set 1/mario.c
@@ -0,0 +1,39 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    int height = 0;
+    
+    do {
+    height = get_int("Please enter the height between 1 and 8: ");
+    if( (height < 1) || (height > 8)  ) {
+        printf("The entered and invalid value \n");
+       
+    } 
+
+    } while( (height < 1) || (height > 8) );
+
+    int loop = height;
+
+    for(int i = 1; i <= loop; i++) {
+        int space = 8 - i;
+
+        for(int j = 0; j < space; j++) {
+            printf(" ");
+        }
+        int hash = 8 - space;
+        for(int h = 0; h < hash; h++) {
+            printf("#");
+
+        }
+             printf(" ");
+        for(int h2 = 0; h2 < hash; h2++) {
+           
+            printf("#");
+        }
+        printf("\n");
+    }
+
+}
