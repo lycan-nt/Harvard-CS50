1657770681fd2f2a06302b5349db09b3509768ca
Felipe D. Santos <lycan.felipe@gmail.com>
2020-08-18 14:43:14 -0300

    Update mario

--- Week 1/Problem Set 1/mario.c
+++ Week 1/Problem Set 1/mario.c
@@ -1,26 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
+//ATUALMENTE TENTANDO ALTERAR A LOGICA DE ESPACOS
+
 int main(void)
 {
 
     int height = 0;
-    
+
     do {
     height = get_int("Please enter the height between 1 and 8: ");
     if( (height < 1) || (height > 8)  ) {
         printf("The entered and invalid value \n");
-       
-    } 
+
+    }
 
     } while( (height < 1) || (height > 8) );
 
     int loop = height;
 
     for(int i = 1; i <= loop; i++) {
-        int space = 8 - i;
+         int space = 8 - i;
+         height = height - 1;
 
-        for(int j = 0; j < space; j++) {
+        for(int j = 0; j < height; j++) {
             printf(" ");
         }
         int hash = 8 - space;
@@ -28,9 +31,9 @@ int main(void)
             printf("#");
 
         }
-             printf(" ");
+             printf("  ");
         for(int h2 = 0; h2 < hash; h2++) {
-           
+
             printf("#");
         }
         printf("\n");
