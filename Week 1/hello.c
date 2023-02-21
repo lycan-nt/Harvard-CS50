d9fca1f640f0a2ac53ca5f10ae58cae2266cdb7b
Felipe D. Santos <lycan.felipe@gmail.com>
2023-02-21 14:40:11 -0300

    Refactoring hello

--- Week 1/hello.c
+++ Week 1/hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
-int main(void) {
-    printf("Hello World...Bora codar!\n");
+int main(void)
+{
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
 }
