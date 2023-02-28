57515611c710f8aabb30567ad6413b56e991127c
Felipe D. Santos <lycan.felipe@gmail.com>
2023-02-28 06:22:58 -0300

    Refactoring Reability

--- Week 2/readability/readability.c
+++ Week 2/readability/readability.c
@@ -3,53 +3,61 @@
 #include <cs50.h>
 #include <math.h>
 
-//Here l e = a letters o w = words and s = a sentences where the n after each e = a numbers
 int ln = 0;
 int wn = 1;
 int sn = 0;
 float Grade = 0;
 
+void analyzeText(string text);
+void calculateAverage();
+void printResult();
+
 int main(void)
 {
-
     string text = get_string("Please enter the text: ");
+    analyzeText(text);
+    calculateAverage();
+    printResult();
+}
 
+//Analyze Text And Count (Letters / Words / Phrases)
+void analyzeText(string text)
+{
     for (int i = 0; text[i] != '\0'; i++)
     {
-
         //Counting letters
         if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
         {
             ln += 1;
         }
-
         //Counting words
         if (ln > 0 && text[i] == ' ')
         {
             wn += 1;
         }
-
         //Counting phrases
         if (wn > 0 && (text[i] == '!'  ||  text[i] == '?'  ||  text[i] == '.'))
         {
             sn += 1;
         }
-
     }
+}
 
+//Calc Media
+void calculateAverage()
+{
     if (ln <= 0)
     {
         wn = 0;
     }
-
-    //Calc Media
     float L = ((float)ln / (float) wn) * 100;
-
     float S = ((float) sn / (float) wn) * 100;
-
-
     Grade = 0.0588 * L - 0.296 * S - 15.8;
+}
 
+//Print Result
+void printResult()
+{
     if (Grade >= 16)
     {
         printf("Grade 16+\n");
@@ -62,5 +70,4 @@ int main(void)
     {
         printf("Grade %.f\n", Grade);
     }
-
 }
